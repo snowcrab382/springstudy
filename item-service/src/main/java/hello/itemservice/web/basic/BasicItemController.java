60de22cb8bea8ff7a40d237204ac24a75b106c6a
임동훈 <108510951+snowcrab382@users.noreply.github.com>
2023-07-16 18:35:53 +0900

    RedirectAttributes

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -74,10 +75,18 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item, Model model) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId()); //쿼리 파라미터로 전달
+        redirectAttributes.addAttribute("status", true); //쿼리 파라미터로 전달
+        return "redirect:/basic/items/{itemId}"; //itemId는 위에서 전달한 값으로 치환됨
     }
 
     @GetMapping("/{itemId}/edit")
